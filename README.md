d891e875012bb5434c2c36d641d116e76746662f
Simon <aguilarsimon01@gmail.com>
2019-12-15 02:22:50 -0300

    I updated the main files of the project

--- README.md
+++ README.md
@@ -1,18 +1,11 @@
-# FreeCodeCamp Timestamp Microservice
-This is a Node.js (with Express.js) little application which is part of the FCC Back End Certification. It takes a date string and gives you back a JSON with Unix value and natural format for the given date.
-> Cheers from Marseille (France), Lior Chamla
+**FreeCodeCamp**- Information Security and Quality Assurance
+------
 
-# User stories:
-> 1. I can pass a string as a parameter, and it will check to see whether that string contains either a unix timestamp or a natural language date (example: January 1, 2016)
-> 2. If it does, it returns both the Unix timestamp and the natural language form of that date.
-> 3. If it does not contain a date or Unix timestamp, it returns null for those properties.
+Project Stock Price Checker
 
-Example usage:
-```
-https://timestamp-microservice-liorchamla.c9users.io/December%2015,%202015
-https://timestamp-microservice-liorchamla.c9users.io/1450137600
-```
-Example output:
-```
-{ "unix": 1450137600, "natural": "December 15, 2015" }
-```
+1) SET NODE_ENV to `test` without quotes and set DB to your mongo connection string
+2) Complete the project in `routes/api.js` or by creating a handler/controller
+3) You will add any security features to `server.js`
+4) You will create all of the functional tests in `tests/2_functional-tests.js`
+
+
--- server.js
+++ server.js
@@ -1,44 +1,78 @@
-/**
- * FreeCodeCamp Timestamp microservice
- * Receive a string parameter and gives a JSON with unix and natural date format
- * @author Lior Chamla
- */
-var http = require('http');
-var path = require('path');
-var express = require('express');
-var strftime = require('strftime');
-var router = express();
-var server = http.createServer(router);
+'use strict';
 
-// static files (html, css ...)
-router.use(express.static(path.resolve(__dirname, 'client')));
+var express     = require('express');
+var bodyParser  = require('body-parser');
+var expect      = require('chai').expect;
+var cors        = require('cors');
+var mongoose    = require('mongoose')
+var helmet      = require('helmet')
 
-// route on GET with a parameter we call :date
-router.get('/:date', function(req, res){
-  // creating a date object
-  var date = new Date();
-  // if the given parameter is a number (timestamp)
-  if(/^\d*$/.test(req.params.date)){
-    date.setTime(req.params.date);
-  } 
-  // else we just create a new date parsing the string given
-  else {
-    date = new Date(req.params.date);
-  }
-  
-  // giving headers for JSON
-  res.set({ 'Content-Type': 'application/json' }) 
-  // if the date is invalid
-  if(!date.getTime()) res.send(JSON.stringify({error: "Invalid date given"}))
-  // else, we send the object with two members (unix and natural)
-  else res.send(JSON.stringify({
-    unix: date.getTime(),
-    natural: strftime('%B %d, %Y', date)
-  }))
+var apiRoutes         = require('./routes/api.js');
+var fccTestingRoutes  = require('./routes/fcctesting.js');
+var runner            = require('./test-runner');
+
+mongoose.Promise = global.Promise
+const dbName = 'stock-price-database'
+mongoose.connect(`${process.env.DB}${dbName}`, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', err => { console.error(err) })
+db.once('open', () => {
+  console.log('Connected to ' + dbName)
 })
 
-// listening to port and processing
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Timestamp microservice listening at", addr.address + ":" + addr.port);
+// Close MongoDB connection
+process.on('SIGINT', () => {
+  db.close(() => {
+    console.log(`Closing connection to ${dbName}`)
+    process.exit(0)
+  })
+})
+
+var app = express();
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use(cors({origin: '*'})); //For FCC testing purposes only
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(helmet())
+
+//Index page (static HTML)
+app.route('/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+//For FCC testing purposes
+fccTestingRoutes(app);
+
+//Routing for API 
+apiRoutes(app);  
+    
+//404 Not Found Middleware
+app.use(function(req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
 });
+
+//Start our server and tests!
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Listening on port " + process.env.PORT);
+  if(process.env.NODE_ENV==='test') {
+    console.log('Running Tests...');
+    setTimeout(function () {
+      try {
+        runner.run();
+      } catch(e) {
+        var error = e;
+          console.log('Tests are not valid:');
+          console.log(error);
+      }
+    }, 3500);
+  }
+});
+
+module.exports = app; //for testing
